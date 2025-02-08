a2845c5354a9a122920eaa72256f3e566e46f77a
Choccymiruku <86089184+Choccymiruku@users.noreply.github.com>
2025-02-09 02:47:36 +0700

    Initial upload

---
+++ Patch.cs
@@ -0,0 +1,48 @@
+﻿using BepInEx;
+using EFT;
+using System;
+using SPT.Reflection.Patching;
+using System.Reflection;
+using UnityEngine;
+using SPT.Reflection.Utils;
+using HarmonyLib;
+using System.Linq;
+
+namespace UpdateHierarchy
+{
+    public class UpdateAnimatorPatch1 : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(Player.FirearmController.GClass1751), "OnMagAppeared");
+        }
+
+        [PatchPostfix]
+        static void PatchPostfix(Player.FirearmController.GClass1751 __instance, Player.FirearmController ___firearmController_0)
+        {
+            if (___firearmController_0 != null)
+            {
+               var weaponPrefab = (WeaponPrefab) AccessTools.Field(___firearmController_0.GetType(), "weaponPrefab_0").GetValue(___firearmController_0);
+                weaponPrefab.UpdateAnimatorHierarchy();
+            }
+        }
+    }
+    
+    public class UpdateAnimatorPatch2 : ModulePatch
+    {
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(Player.FirearmController.GClass1773), "OnMagAppeared");
+        }
+
+        [PatchPostfix]
+        static void PatchPostfix(Player.FirearmController.GClass1773 __instance, Player.FirearmController ___firearmController_0)
+        {
+            if (___firearmController_0 != null)
+            {
+                var weaponPrefab = (WeaponPrefab) AccessTools.Field(___firearmController_0.GetType(), "weaponPrefab_0").GetValue(___firearmController_0);
+                weaponPrefab.UpdateAnimatorHierarchy();
+            }
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+
+namespace UpdateHierarchy
+{
+    [BepInPlugin("com.choccy.updatehierarchy", "Choccy.AnimatorUpdate", "1.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo("Animator Updater is loaded");
+            new UpdateAnimatorPatch1().Enable();
+            new UpdateAnimatorPatch2().Enable();
+        }
+    }
+}
