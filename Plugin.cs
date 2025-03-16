270f199d3e531040266c51ff79d7725eb558428e
Choccy <yarlouz10@gmail.com>
2025-03-17 04:09:38 +0700

    Version bump

--- Plugin.cs
+++ Plugin.cs
@@ -2,7 +2,7 @@
 
 namespace UpdateHierarchy
 {
-    [BepInPlugin("com.choccy.updatehierarchy", "Choccy.AnimatorUpdate", "1.0.0")]
+    [BepInPlugin("com.choccy.updatehierarchy", "Choccy.AnimatorUpdate", "1.0.2")]
     public class Plugin : BaseUnityPlugin
     {
         private void Awake()
